814dd02e71d91f52fcd165bd5b79c8a190aeedd8
Anikesh Roy <anikeshroy62040@gmail.com>
2026-06-11 11:56:01 +0530

    updated variable file

--- 1_variable.js
+++ 1_variable.js
@@ -1,1 +1,10 @@
 console.log("Hii");
+
+let age = 25;
+console.log(`Your age is ${age}`);
+console.log(`You are ${age} years old`);
+
+let name = "Anikesh";
+console.log(`Your name is ${name}`);
+
+console.log(`Your name is ${name} and Your age is ${age}`);

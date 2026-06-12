40a06967dbbc843a3cf8e211d94e8cb7e81753ba
Anikesh Roy <anikeshroy62040@gmail.com>
2026-06-12 09:50:21 +0530

    feat: add example of data types

---
+++ 02_data_types.js
@@ -0,0 +1,10 @@
+let name = "Anikesh Roy";
+let age = 23;
+let isStudent = true;
+let college = "GEC Jamui";
+let department = "CSE";
+
+console.log(`My name is: ${name}`);
+console.log(`I'm ${age} years old`);
+console.log(`I have studied ${department} from ${college}`);
+console.log(`I'm student: ${isStudent}`);

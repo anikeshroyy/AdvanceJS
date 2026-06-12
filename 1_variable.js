50f803637586687a248afb80f361432bbd0fc1e3
Anikesh Roy <anikeshroy62040@gmail.com>
2026-06-12 09:28:53 +0530

    added boolean var

--- 1_variable.js
+++ 1_variable.js
@@ -7,4 +7,10 @@ console.log(`You are ${age} years old`);
 let name = "Anikesh";
 console.log(`Your name is ${name}`);
 
+
 console.log(`Your name is ${name} and Your age is ${age}`);
+
+
+let isStudent = true;
+console.log(typeof(isStudent));
+console.log(`Rahul is student: ${isStudent}`);

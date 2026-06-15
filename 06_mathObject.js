9db1de464022408546f32ef15b3cb1c6481a4972
Anikesh Roy <anikeshroy62040@gmail.com>
2026-06-15 11:38:20 +0530

    feat: add example of math object

---
+++ 06_mathObject.js
@@ -0,0 +1,23 @@
+let x = 10.55;
+let y = 2;
+
+let z;
+
+z = Math.sqrt(x)
+console.log(z)
+
+
+// Math.round(10.55);                   // 11
+// Math.floor(10.55);                   // 10
+// Math.ceil(10.55);                    // 11
+// Math.trunc(10.55);                   // 10
+
+// Math.pow(2, 3);                      // 8
+// Math.sqrt(25);                       // 5
+
+// Math.abs(-10);                       // 10
+
+// Math.max(10, 20, 30);                // 30
+// Math.min(10, 20, 30);                // 10
+
+// Math.PI;                             // 3.141592653589793

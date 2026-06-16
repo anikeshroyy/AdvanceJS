cbfb42f72e22cc2a2eae413c27b8652dd2e60947
Anikesh Roy <anikeshroy62040@gmail.com>
2026-06-16 14:00:53 +0530

    feat: add example of ternary operator

--- 03_operator.js
+++ 03_operator.js
@@ -1,34 +1,51 @@
+// -------------------------- Assignment Operators -------------------
+
 let student = 30;
 student += 1;
 console.log(student);
 
-let age = 20;
-age -= 2;
-console.log(age);
 
-let mul = 2;
-mul *= 2;
-console.log(mul);
+// -------------------------- Operator Precedence --------------------
 
-let div = 32;
-div /= 4;
-console.log(div);
-
-let set = 40;
-set %= 4;
-console.log(set);
-
-
-// operator precedence
-// 1. parenthesis()
-// 2. exponents
-// 3. modulo, division, multiplicaton
-// 4. addition substraction
-// when operator has same precedence thent it was treated from left to right (% / *)
-
-let ran = 44+3-(12*2)/2%2;
+let ran = 44 + 3 - (12 * 2) / 2 % 2;
 console.log(ran);
+
+
+// -------------------------- Exponent Operator ----------------------
 
 let result = 3;
 result = result ** 2;
 console.log(result);
+
+
+// -------------------------- Ternary Operator -----------------------
+
+let age = 25;
+let message = age >= 18 ? "You can vote" : "You can't vote";
+console.log(message);
+
+
+// -------------------------- Greeting Example -----------------------
+
+let time = 16;
+let greetings = time >= 12 ? "Good Afternoon" : "Good Morning";
+console.log(greetings);
+
+
+// -------------------------- Student Status --------------------------
+
+let isStudent = false;
+let msg = isStudent ? "You are a student" : "You are not a student";
+console.log(msg);
+
+
+// -------------------------- Discount Calculator ---------------------
+
+let purchaseAmount = 125;
+let discount = purchaseAmount >= 100 ? 10 : 0;
+
+console.log(
+    `You purchased products worth $${purchaseAmount}.
+Discount: ${discount}%.
+Final Amount: $${purchaseAmount - purchaseAmount * (discount / 100)}`
+);

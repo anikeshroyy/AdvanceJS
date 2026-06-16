5315a3247db2f9ce9eef5aa3b8d543cc7a79031f
Anikesh Roy <anikeshroy62040@gmail.com>
2026-06-16 08:57:08 +0530

    feat: add NaN handling logic

--- project/03_voteChecker/index.js
+++ project/03_voteChecker/index.js
@@ -1,8 +1,12 @@
 let age;
 
 document.getElementById("checkBtn").onclick = function () {
-    age = document.getElementById("ageInput").value;
-    if (age >= 18) {
+    age = Number(document.getElementById("ageInput").value);
+    if (isNaN(age)) {
+        document.getElementById("dispResult").textContent =
+            "Please enter a valid age!";
+    }
+    else if (age >= 18) {
         document.getElementById("dispResult").textContent = `Yes, you can vote!`;
         document.getElementById("dispResult").style.color = "green";
     }

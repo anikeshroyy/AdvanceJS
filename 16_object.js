a3ca364b4e9b90226e31a6c9d8604cff38cebdcf
Anikesh Roy <anikeshroy62040@gmail.com>
2026-06-23 17:35:02 +0530

    feat: add an example of nested objects

--- 16_object.js
+++ 16_object.js
@@ -27,3 +27,35 @@ console.log(person.lastName);
 console.log(person.age);
 console.log(person.email);
 person.sayHi()
+
+const customer = {
+    fullName: "Anikesh Roy",
+    userName: "aniraj972",
+    isOldCustomer: true,
+    cart: ["keyboard", "mouse", "monitor"],
+    address: {
+        street: "paschim tola",
+        village: "suriyahi",
+        district: "madhubani",
+        pinCode: 847409,
+    },
+    paymentMethod: {
+        method1: "Upi",
+        method2: "Card",
+    },
+    completePurchase: () => { console.log("Order Placed") },
+}
+
+console.log(`customer full name is : `, customer.fullName);
+console.log(`customer user name is : `, customer.userName);
+console.log(`is old customer? : `, customer.isOldCustomer);
+console.log(`cart item : `, customer.cart);
+console.log(`cart 2nd item : `, customer.cart[1]);
+console.log(`customer address is : `, customer.address);
+console.log(`customer street address is : `, customer.address.street);
+console.log(`available payment method :`, customer.paymentMethod);
+customer.completePurchase();
+
+for (const property in customer.address){
+    console.log(customer.address[property])
+}

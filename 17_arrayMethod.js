43887ac132af0a827ca3c5d7a8a8635e1a536d46
Anikesh Roy <anikeshroy62040@gmail.com>
2026-06-25 08:07:33 +0530

    feat: add example of find method

--- 17_arrayMethod.js
+++ 17_arrayMethod.js
@@ -115,9 +115,15 @@ console.table(books.filter((bk) => bk.publishYear >= 2018 && bk.publishYear <= 2
 console.table(books.filter((bk) => bk.genre !== "Programming"))
 
 console.log("-----------------------map()----------------------")
-
 const arrayOfNumbers = [5, 7, 2, 4, 11, 6, 3, 5, 8, 12, 10];
 console.log(arrayOfNumbers.map((numArr) => numArr * 2));
 console.table(arrayOfNumbers);
 console.table(arrayOfNumbers.map((numArr) => numArr + 5).filter((numbersArr) => numbersArr >= 15));
 console.log(arrayOfNumbers.map((numArr) => { return numArr * 2 - 2 }).filter((numArr) => { return numArr >= 5 }))
+
+
+console.log("-----------------------find()----------------------")
+console.log(books.find((books) => books.edition > 1))
+console.log(books.find((books) => books.genre === "Programming"))
+console.log(books.find((books) => books.publishYear > 2022))
+console.log(books.find((books) => books.genre === "Fantasy"))

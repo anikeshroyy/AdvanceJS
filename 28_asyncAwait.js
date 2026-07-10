4181a35f7015a866a1fdd589807103eabbcf120f
Anikesh Roy <anikeshroy62040@gmail.com>
2026-07-10 10:08:18 +0530

    learnt: how await work in js

--- 28_asyncAwait.js
+++ 28_asyncAwait.js
@@ -1,12 +1,30 @@
-async function getData() {
-    return pr;
-    console.log("Hello World")
-}
+// async function getData() {
+//     return pr;
+//     console.log("Hello World")
+// }
+
+
+// getData().then((data) => {
+//     console.log(data)
+// })
+
 
 const pr = new Promise((resolve, reject) => {
-    resolve(2 + 2);
+    console.log("Pr Start")
+
+    setTimeout(() => {
+        resolve(2 + 2);
+    }, 3000);
+
+    console.log("Pr End")
 })
 
-getData().then((data) => {
-    console.log(data)
-})
+const newFn = async () => {
+    console.log("Fn Start")
+    const newPromise = await pr;
+    console.log(newPromise);
+    console.log("Fn End")
+
+}
+
+newFn();

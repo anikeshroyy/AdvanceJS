9a0e5acf2cdcf8d1c1b93b5935e6a4385fec6250
Anikesh Roy <anikeshroy62040@gmail.com>
2026-07-13 10:51:30 +0530

    feat: add js of current time and date

--- project/14_weatherDashboard/index.js
+++ project/14_weatherDashboard/index.js
@@ -3,3 +3,30 @@ const themeBtn = document.getElementById("themeIcon");
 themeBtn.addEventListener("click", () => {
     document.documentElement.classList.toggle("light-theme");
 })
+
+// Current Date And Time -------------------------------------------
+const currentTime = document.getElementById("time");
+const currentDate = document.getElementById("date")
+
+function updateDateTime() {
+    const now = new Date();
+
+    currentDate.textContent = now.toLocaleDateString("en-In", {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+    });
+    currentTime.textContent = now.toLocaleTimeString();
+}
+
+setInterval(updateDateTime, 1000)
+
+// City Search --------------------------------------------------------------
+const searchCity = document.getElementById("cityQuery");
+const searchBtn = document.getElementById("srchBtn");
+
+searchBtn.onclick = function () {
+    const searchVal = (searchCity.value.trim())
+    document.getElementById("city").textContent = `${searchVal}`;
+}
